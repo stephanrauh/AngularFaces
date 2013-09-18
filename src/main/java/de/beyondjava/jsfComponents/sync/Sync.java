dd1bdf30b6c30208df51d41cf5c0e67cd99d712f
stephanrauh <stephan.rauh@gmx.net>
2013-09-18 22:30:15 +0200

    simplified a:sync

--- src/main/java/de/beyondjava/jsfComponents/sync/Sync.java
+++ src/main/java/de/beyondjava/jsfComponents/sync/Sync.java
@@ -1,12 +1,14 @@
 package de.beyondjava.jsfComponents.sync;
 
 import java.io.IOException;
+import java.util.List;
 
-import javax.el.*;
+import javax.el.ValueExpression;
 import javax.faces.application.Application;
-import javax.faces.component.FacesComponent;
-import javax.faces.component.html.HtmlInputHidden;
+import javax.faces.component.*;
+import javax.faces.component.html.HtmlInputText;
 import javax.faces.context.FacesContext;
+import javax.faces.event.*;
 
 import de.beyondjava.jsfComponents.common.ELTools;
 
@@ -18,34 +20,38 @@ import de.beyondjava.jsfComponents.common.ELTools;
  */
 
 @FacesComponent("de.beyondjava.Sync")
-public class Sync extends org.primefaces.component.inputtext.InputText {
+public class Sync extends org.primefaces.component.inputtext.InputText implements SystemEventListener {
    public static final String COMPONENT_FAMILY = "org.primefaces.component";
 
    /**
-    * Analyzes the value attributes, explodes it to a list of attributes and
-    * adds a hidden input text for each bean attribute.
+    * This constructor subscribes to the PreRenderViewEvent. Catching the
+    * PreRenderViewEvent allows AngularFaces to modify the JSF tree by adding a
+    * label and a message.
     */
+   public Sync() {
+      FacesContext context = FacesContext.getCurrentInstance();
+      UIViewRoot root = context.getViewRoot();
+      root.subscribeToViewEvent(PreRenderViewEvent.class, this);
+   }
+
+   @Override
+   public void decode(FacesContext context) {
+      // Map<String, String> params =
+      // context.getExternalContext().getRequestParameterMap();
+      // List<String> everyProperty =
+      // ELTools.getEveryProperty(ELTools.getCoreValueExpression(this), false);
+      // for (String property : everyProperty) {
+      //
+      // String id = (getClientId() + property).replace(".", "").replace(":",
+      // "");
+      // String value = params.get(id);
+      // setSubmittedValue(value);
+      // }
+   }
+
    @Override
    public void encodeBegin(FacesContext context) throws IOException {
-      Application app = FacesContext.getCurrentInstance().getApplication();
-
-      ValueExpression valueExpression = getValueExpression("value");
-      ELContext elContext = FacesContext.getCurrentInstance().getELContext();
-      // Object value = valueExpression.getValue(elContext);
-      // String v = valueExpression.getExpressionString();
-
-      HtmlInputHidden grid = (HtmlInputHidden) app.createComponent("javax.faces.HtmlInputHidden");
-      // HtmlInputText grid = (HtmlInputText)
-      // app.createComponent("javax.faces.HtmlInputText");
-      grid.setValueExpression("value", valueExpression);
-      String attributeName = ELTools.getCoreValueExpression(this);
-      if (attributeName.contains(".")) {
-         int pos = attributeName.lastIndexOf('.');
-         attributeName = attributeName.substring(pos + 1);
-      }
-
-      grid.getPassThroughAttributes(true).put("ng-model", attributeName);
-      grid.encodeAll(context);
+      encodeChildren(context);
    }
 
    @Override
@@ -60,4 +66,36 @@ public class Sync extends org.primefaces.component.inputtext.InputText {
    public String getType() {
       return "hidden";
    }
+
+   @Override
+   public boolean isListenerForSource(Object source) {
+      return (source instanceof UIViewRoot);
+   }
+
+   /**
+    * Catching the PreRenderViewEvent allows AngularFaces to modify the JSF tree
+    * by adding a label and a message.
+    */
+   @Override
+   public void processEvent(SystemEvent event) throws AbortProcessingException {
+      FacesContext context = FacesContext.getCurrentInstance();
+      if (!context.isPostback()) {
+         Application app = context.getApplication();
+
+         String rootProperty = ELTools.getCoreValueExpression(this);
+         int beanNameIndex = rootProperty.lastIndexOf('.');
+         List<String> everyProperty = ELTools.getEveryProperty(rootProperty, false);
+         for (String property : everyProperty) {
+            HtmlInputText sychronizingHiddenInput = (HtmlInputText) app.createComponent("javax.faces.HtmlInputText");
+            ValueExpression valueExpression = ELTools.createValueExpression("#{" + property + "}");
+            sychronizingHiddenInput.setValueExpression("value", valueExpression);
+            sychronizingHiddenInput.setId((getClientId() + property).replace(".", "").replace(":", ""));
+            sychronizingHiddenInput.getPassThroughAttributes(true).put("ng-model",
+                  property.substring(beanNameIndex + 1));
+            sychronizingHiddenInput.getAttributes().put("name", property);
+            getChildren().add(sychronizingHiddenInput);
+         }
+      }
+   }
+
 }
