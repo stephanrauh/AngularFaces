00fdf0fef44a986d9a438e0ef5696c957e7a6651
stephanrauh <stephan.rauh@gmx.net>
2013-12-04 00:13:05 +0100

    - added a primefaces example to the optimize differential AJAX engine - updated to PrimeFaces 4.1 SNAPSHOT (SVN Revision 10308)

---
+++ createPrimefacesMavenRepo.bat
@@ -0,0 +1,1 @@
+mvn install:install-file -DlocalRepositoryPath=lib/ -DcreateChecksum=true -Dpackaging=jar -Dfile=primefaces-4.1-SNAPSHOT.jar -DgroupId=org.primefaces -DartifactId=primefaces -Dversion=4.1-SNAPSHOT
