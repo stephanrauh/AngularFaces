88d2fd4f44831f0eff3d2f2bb1f77e7d7b57a96b
stephanrauh <stephan@beyondjava.de>
2014-04-19 18:43:14 +0200

    optimized project setup

--- AngularFaces_1.0/AngularFaces-core/createPrimefacesMavenRepo.bat
+++ AngularFaces_1.0/AngularFaces-core/createPrimefacesMavenRepo.bat
@@ -1,1 +1,5 @@
-mvn install:install-file -DlocalRepositoryPath=lib/ -DcreateChecksum=true -Dpackaging=jar -Dfile=primefaces-4.1-SNAPSHOT.jar -DgroupId=org.primefaces -DartifactId=primefaces -Dversion=4.1-SNAPSHOT
+REM mvn install:install-file -DlocalRepositoryPath=lib/ -DcreateChecksum=true -Dpackaging=jar -Dfile=primefaces-4.1-SNAPSHOT.jar -DgroupId=org.primefaces -DartifactId=primefaces -Dversion=4.1-SNAPSHOT
+
+mvn install:install-file -DlocalRepositoryPath=lib -DcreateChecksum=true -Dpackaging=jar -Dfile=primefaces-5.0-SNAPSHOT.jar -DgroupId=org.primefaces -DartifactId=primefaces -Dversion=5.0-SNAPSHOT
+
+pause
