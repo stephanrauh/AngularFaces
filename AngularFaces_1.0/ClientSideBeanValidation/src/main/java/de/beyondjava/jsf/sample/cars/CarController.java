9d0662c962b09b3bbd9692e766d28d81c58a4812
stephanrauh <stephan.rauh@gmx.net>
2014-05-07 22:17:29 +0200

    started to implement PF5 Client Side Bean Validations

---
+++ AngularFaces_1.0/ClientSideBeanValidation/src/main/java/de/beyondjava/jsf/sample/cars/CarController.java
@@ -0,0 +1,38 @@
+/**
+ *  (C) 2013-2014 Stephan Rauh http://www.beyondjava.net
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package de.beyondjava.jsf.sample.cars;
+
+import java.util.logging.Logger;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class CarController {
+    private static final Logger LOGGER = Logger.getLogger("de.beyondjava.jsf.sample.cars.CarController");
+
+    /**
+     *
+     */
+    public CarController() {
+    }
+
+    public String save() {
+        LOGGER.info("save action called");
+        return "table.jsf";
+    }
+
+}
