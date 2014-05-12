fcae5fca2f498b19cf820a30d78a24f967b879ad
stephanrauh <stephan.rauh@gmx.net>
2014-05-12 21:55:54 +0200

    added documentation to the examples

--- AngularFaces_1.0/babbageFaces-examples/src/main/java/de/beyondjava/jsf/sample/differenceEngine/DifferenceDemoBean.java
+++ AngularFaces_1.0/babbageFaces-examples/src/main/java/de/beyondjava/jsf/sample/differenceEngine/DifferenceDemoBean.java
@@ -5,12 +5,12 @@
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -169,10 +169,15 @@ public class DifferenceDemoBean implements Serializable {
         return "ok";
     }
 
-    /** Called by the Mojarra example when the third check box is clicked. */
     public void toggleFirstAndThirdSection(javax.faces.event.AjaxBehaviorEvent event) {
+        firstSectionVisible = !firstSectionVisible;
+        thirdSectionVisible = !thirdSectionVisible;
+    }
+
+    public void toggleFirstAndThirdSectionPrime() {
         thirdSectionVisible = !thirdSectionVisible;
         firstSectionVisible = !firstSectionVisible;
+
     }
 
     /** Called by the Mojarra example when the first check box is clicked. */
@@ -181,7 +186,7 @@ public class DifferenceDemoBean implements Serializable {
     }
 
     public void toggleFirstSectionPrime() {
-        // firstSectionVisible = !firstSectionVisible;
+        firstSectionVisible = !firstSectionVisible;
     }
 
     public void toggleSecondSectionAndChangeCity() {
@@ -195,14 +200,9 @@ public class DifferenceDemoBean implements Serializable {
         changeCity();
     }
 
+    /** Called by the Mojarra example when the third check box is clicked. */
     public void toggleThirdSection(javax.faces.event.AjaxBehaviorEvent event) {
-        firstSectionVisible = !firstSectionVisible;
-    }
-
-    public void toggleThirdSectionPrime() {
-        // thirdSectionVisible = !thirdSectionVisible;
-        firstSectionVisible = !firstSectionVisible;
-
+        thirdSectionVisible = !thirdSectionVisible;
     }
 
     public void valueChangeListener(ValueChangeEvent event) {
