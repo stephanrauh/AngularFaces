935cf8a9e10d511e14f032adada3562538e379f7
stephanrauh <stephan.rauh@gmx.net>
2014-08-08 23:02:58 +0200

    fixed case

--- AngularFaces_2.0/AngularFaces-core/src/main/java/de/beyondjava/angularFaces/flavors/kendo/puiGridLayout/PuiGridRenderer.java
+++ AngularFaces_2.0/AngularFaces-core/src/main/java/de/beyondjava/angularFaces/flavors/kendo/puiGridLayout/PuiGridRenderer.java
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package de.beyondjava.angularFaces.flavors.kendo.PuiGrid;
+package de.beyondjava.angularFaces.flavors.kendo.puiGridLayout;
 
 import java.io.IOException;
 import java.util.List;
