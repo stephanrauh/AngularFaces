f70123878653e6254146bb85a77d1a273c826f6c
stephanrauh <stephan.rauh@gmx.net>
2014-08-22 22:33:35 +0200

    simplified Javascript code

--- AngularFaces_2.0/AngularFaces-2.0-examples/src/main/webapp/manual/2_ngtable/main.js
+++ AngularFaces_2.0/AngularFaces-2.0-examples/src/main/webapp/manual/2_ngtable/main.js
@@ -2,22 +2,19 @@ var app = angular.module('AngularFacesExamples', ["angularfaces", 'ngTable']).
 controller('MyCtrl', function($scope, $filter, ngTableParams) {
 	// This initializes the Angular Model with the values of the JSF bean attributes
 	initJSFScope($scope);
-    var data = $scope.carPool.carPool;
 
     $scope.tableParams = new ngTableParams({
-        page: 1,            // show first page
-        count: 10,           // count per page
-        sorting: {
-            brand: 'asc'     // initial sorting
-        }
+        page: 1,                  // show first page
+        count: 10,                // count per page
+        sorting: { brand: 'asc' } // initial sorting
     }, {
-        total: data.length, // length of data
+        total: $scope.carPool.carPool.length, // length of data
         getData: function($defer, params) {
-            var orderedData = params.sorting() ?
-                    $filter('orderBy')(data, params.orderBy()) :
-                    data;
-
-			$defer.resolve(orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count()));
+        	var rows = $scope.carPool.carPool;
+        	if (params.sorting()) rows = $filter('orderBy')(rows, params.orderBy());
+        	var page = params.page();
+        	var pageLength = params.count();
+			$defer.resolve(rows.slice((page - 1) * pageLength, page * pageLength));
         }
     });
 })
