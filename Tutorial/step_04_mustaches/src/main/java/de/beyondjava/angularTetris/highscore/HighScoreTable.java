8635324fe34a24c5eef4896c174aa2762b1961a5
stephanrauh <stephan@beyondjava.de>
2014-08-31 12:55:32 +0200

    added empty template for the tutorial projects

---
+++ Tutorial/step_04_mustaches/src/main/java/de/beyondjava/angularTetris/highscore/HighScoreTable.java
@@ -0,0 +1,13 @@
+package de.beyondjava.angularTetris.highscore;
+
+import java.io.Serializable;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean
+@SessionScoped
+public class HighScoreTable implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+}
---
+++ Tutorial/step_05_bean_validation_api/src/main/java/de/beyondjava/angularTetris/settings/Settings.java
@@ -0,0 +1,48 @@
+package de.beyondjava.angularTetris.settings;
+
+import java.io.Serializable;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean
+@SessionScoped
+public class Settings implements Serializable {
+	private static final long serialVersionUID = 1L;
+	private int numberOfRows = 25;
+	private int numberOfColumns = 10;
+	private boolean preview = false;
+	private boolean ignoreGravitiy = true;
+
+	public int getNumberOfRows() {
+		return numberOfRows;
+	}
+
+	public void setNumberOfRows(int numberOfRows) {
+		this.numberOfRows = numberOfRows;
+	}
+
+	public int getNumberOfColumns() {
+		return numberOfColumns;
+	}
+
+	public void setNumberOfColumns(int numberOfColumns) {
+		this.numberOfColumns = numberOfColumns;
+	}
+
+	public boolean isPreview() {
+		return preview;
+	}
+
+	public void setPreview(boolean preview) {
+		this.preview = preview;
+	}
+
+	public boolean isIgnoreGravitiy() {
+		return ignoreGravitiy;
+	}
+
+	public void setIgnoreGravitiy(boolean ignoreGravitiy) {
+		this.ignoreGravitiy = ignoreGravitiy;
+	}
+}
