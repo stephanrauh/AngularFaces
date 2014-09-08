1e2718286723d613ec3334cf9f4815674c662af3
stephanrauh <stephan@beyondjava.de>
2014-09-08 19:58:39 +0200

    fixed syntaxhighlighting of the tutorial

--- AngularFaces.com/WebContent/controllers.js
+++ AngularFaces.com/WebContent/controllers.js
@@ -2,7 +2,20 @@
 
 var tutorialControllers = angular.module('tutorialControllers', []);
 
-tutorialControllers.controller('EmptyController', ['$scope', '$routeParams',
-  function($scope, $routeParams) {
-    $scope.tutorialId = $routeParams.tutorialId;
-  }]);
+tutorialControllers.controller('EmptyController', [ '$scope', '$routeParams',
+		function($scope, $routeParams) {
+			$scope.tutorialId = $routeParams.tutorialId;
+
+			$scope.highlight = function() {
+				try {
+					var codeblocks = document.getElementsByTagName('pre');
+					if (null != codeblocks) {
+					for (var i = 0; i < codeblocks.length;i++)
+						hljs.highlightBlock(codeblocks[i]);
+					}
+
+				} catch (e) {
+					alert("error");
+				}
+			};
+		} ]);
