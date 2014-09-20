1ba2b0b08974f50e99f239d15847c95bd023b23d
stephanrauh <stephan.rauh@gmx.net>
2014-09-21 01:15:41 +0200

    split the JSF pages into several smaller JSF pages

---
+++ AngularFaces_2.0/AngularFaces-2.0-examples/src/main/java/de/beyondjava/jsf/sample/carshop/NewCarBean.java
@@ -0,0 +1,38 @@
+package de.beyondjava.jsf.sample.carshop;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.view.ViewScoped;
+
+@ManagedBean
+@ViewScoped
+public class NewCarBean {
+	private Car car = new Car(null, null, 0, null, 0, null, 0);
+
+	public String getBrand() {
+		return car.getBrand();
+	}
+
+	public String getType() {
+		return car.getType();
+	}
+
+	public String getColor() {
+		return car.getColor();
+	}
+
+	public int getPrice() {
+		return car.getPrice();
+	}
+
+	public int getMileage() {
+		return car.getMileage();
+	}
+
+	public int getYear() {
+		return car.getYear();
+	}
+	
+	public String getFuel() {
+		return car.getFuel();
+	}
+}
---
+++ AngularFaces_2.0/AngularFaces-2.0-examples/src/main/webapp/carshop/addCar.js
@@ -0,0 +1,37 @@
+var app = angular.module('CarShop', [ "angularfaces" ]).controller(
+		'CarShopController', function($scope) {
+			// This initializes the Angular Model with the values of the JSF
+			// bean attributes
+			initJSFScope($scope);
+
+			$scope.$watch('filterBean.brand', sendFilterToServer);
+			$scope.$watch('filterBean.type', sendFilterToServer);
+			$scope.$watch('filterBean.price', sendFilterToServer);
+			$scope.$watch('filterBean.mileage', sendFilterToServer);
+			$scope.$watch('filterBean.fuel', sendFilterToServer);
+			$scope.$watch('filterBean.color', sendFilterToServer);
+			$scope.$watch('filterBean.yearText', sendFilterToServer);
+
+		})
+
+function sendFilterToServer(newValue, oldValue) {
+	if (newValue != oldValue) {
+		try {
+			
+			var ngsync = document.getElementsByClassName('filterSyncClass')[0];
+			
+			if (event.target.nextElementSibling) {
+				var helperID = ngsync.id;
+				window.setTimeout(function() {
+					jsf.ajax.request(ngsync.id, null, {
+						'javax.faces.behavior.event':'valueChange', 
+						execute:helperID,
+						render:'angular'
+					});
+				}, 10);
+			}
+		} catch (e) {
+			console.log("Ein Fehler ist aufgetreten: " + e);
+		}
+	}
+}
