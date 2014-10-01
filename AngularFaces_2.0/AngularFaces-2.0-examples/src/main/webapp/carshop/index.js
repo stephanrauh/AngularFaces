3ad2cff3b66c2c758431942855bc1c6f37dce93e
stephanrauh <stephan.rauh@gmx.net>
2014-10-01 22:44:52 +0200

    started to experiment with OmniFaces resource management and JS API for ngsync (work in progress)

--- AngularFaces_2.0/AngularFaces-2.0-examples/src/main/webapp/carshop/index.js
+++ AngularFaces_2.0/AngularFaces-2.0-examples/src/main/webapp/carshop/index.js
@@ -3,35 +3,33 @@ var app = angular.module('CarShop', [ "angularfaces" ]).controller(
 			// This initializes the Angular Model with the values of the JSF
 			// bean attributes
 			initJSFScope($scope);
+			
+			$scope.sendFilterToServer = function(newValue, oldValue) {
+				if (newValue != oldValue) {
+					try {
+						var ngsync = document.getElementsByClassName('filterSyncClass')[0];
+						sendNGSyncToServer(ngsync.id);
+					} catch (e) {
+						console.log("Ein Fehler ist aufgetreten: " + e);
+					}
+				}
+			};
 
-			$scope.$watch('filterBean.brand', sendFilterToServer);
-			$scope.$watch('filterBean.type', sendFilterToServer);
-			$scope.$watch('filterBean.price', sendFilterToServer);
-			$scope.$watch('filterBean.mileage', sendFilterToServer);
-			$scope.$watch('filterBean.fuel', sendFilterToServer);
-			$scope.$watch('filterBean.color', sendFilterToServer);
-			$scope.$watch('filterBean.yearText', sendFilterToServer);
-
+			$scope.$watch('filterBean.brand', $scope.sendFilterToServer);
+			$scope.$watch('filterBean.type', $scope.sendFilterToServer);
+			$scope.$watch('filterBean.price', $scope.sendFilterToServer);
+			$scope.$watch('filterBean.mileage', $scope.sendFilterToServer);
+			$scope.$watch('filterBean.fuel', $scope.sendFilterToServer);
+			$scope.$watch('filterBean.color', $scope.sendFilterToServer);
+			$scope.$watch('filterBean.yearText', $scope.sendFilterToServer);
 		})
 
-function sendFilterToServer(newValue, oldValue) {
-	if (newValue != oldValue) {
-		try {
-			
-			var ngsync = document.getElementsByClassName('filterSyncClass')[0];
-			
-			if (event.target.nextElementSibling) {
-				var helperID = ngsync.id;
-				window.setTimeout(function() {
-					jsf.ajax.request(ngsync.id, null, {
-						'de.beyondjava.angularfaces.behavior.event':'ngsync', 
-						execute:helperID,
-						render:'angular'
-					});
-				}, 10);
-			}
-		} catch (e) {
-			console.log("Ein Fehler ist aufgetreten: " + e);
-		}
-	}
+function sendNGSyncToServer(ngsyncID) {
+	window.setTimeout(function() {
+		jsf.ajax.request(ngsyncID, null, {
+			'de.beyondjava.angularfaces.behavior.event':'ngsync', 
+			execute:ngsyncID,
+			render:'angular'
+		});
+	}, 10);
 }
