38c22d2598f9a586ffb386b6d0383a1565c05f0f
stephanrauh <stephan.rauh@gmx.net>
2015-02-23 19:59:09 +0100

    improved the detection whether AngularFaces runs on Mojarra or MyFaces on installations that have both libraries installed

--- AngularFaces_2.0/AngularFaces-core/src/main/java/de/beyondjava/angularFaces/core/Configuration.java
+++ AngularFaces_2.0/AngularFaces-core/src/main/java/de/beyondjava/angularFaces/core/Configuration.java
@@ -1,14 +1,28 @@
 package de.beyondjava.angularFaces.core;
 
 public class Configuration {
-	public static boolean bootsFacesActive = false;
-	static {
-		try {
-			Class.forName("net.bootsfaces.layout.Column");
-			bootsFacesActive=true;
-		} catch (Exception doWithoutBootsFaces) {
-			// Bootsfaces is not active
-		}
-	}
+    public static boolean bootsFacesActive = false;
+    public static boolean myFaces = false;
+
+    static {
+        try {
+            Class.forName("net.bootsfaces.layout.Column");
+            bootsFacesActive = true;
+        } catch (Exception doWithoutBootsFaces) {
+            // Bootsfaces is not active
+        }
+        StackTraceElement[] stackTrace = new NullPointerException().getStackTrace();
+        for (StackTraceElement line : stackTrace) {
+            if (line.getClassName().contains("org.apache.myfaces")) {
+                myFaces = true;
+                break;
+            }
+            if (line.getClassName().contains("com.sun.faces")) {
+                myFaces = false;
+                break;
+            }
+        }
+
+    }
 
 }
--- AngularFaces_2.0/AngularFaces-core/src/main/java/de/beyondjava/angularFaces/core/tagTransformer/TagAttributeUtilities.java
+++ AngularFaces_2.0/AngularFaces-core/src/main/java/de/beyondjava/angularFaces/core/tagTransformer/TagAttributeUtilities.java
@@ -20,22 +20,26 @@ import java.lang.reflect.Constructor;
 import javax.faces.view.Location;
 import javax.faces.view.facelets.TagAttribute;
 
+import de.beyondjava.angularFaces.core.Configuration;
+
 /** Create a tag attribute by means of reflection (so that they work with both Mojarra and MyFaces). */
 public class TagAttributeUtilities {
-    public static TagAttribute createTagAttribute(Location location, String ns, String myLocalName, String qName, String value) {
-    	try {
-    		Class<?> implClass;
-    		try {
-    	implClass = Class.forName("org.apache.myfaces.view.facelets.tag.TagAttributeImpl");
-    		} catch (Exception e) {
-    	implClass = Class.forName("com.sun.faces.facelets.tag.TagAttributeImpl");
-    		}
-    		Constructor<?> constructor = implClass.getConstructor(Location.class, String.class, String.class, String.class, String.class);
-    		Object newTagAttribute = constructor.newInstance(location, ns, myLocalName, qName, value);
-			return (TagAttribute) newTagAttribute;
-    	} catch (ReflectiveOperationException e) {
-    		throw new IllegalArgumentException("Couldn't create neither a Oracle Mojarra Tag attribute nor an Apache MyFaces TagAttribute", e);
-    	}
+    public static TagAttribute createTagAttribute(Location location, String ns, String myLocalName, String qName,
+            String value) {
+        try {
+            Class<?> implClass;
+            if (Configuration.myFaces)
+                implClass = Class.forName("org.apache.myfaces.view.facelets.tag.TagAttributeImpl");
+            else
+                implClass = Class.forName("com.sun.faces.facelets.tag.TagAttributeImpl");
+            Constructor<?> constructor = implClass.getConstructor(Location.class, String.class, String.class,
+                    String.class, String.class);
+            Object newTagAttribute = constructor.newInstance(location, ns, myLocalName, qName, value);
+            return (TagAttribute) newTagAttribute;
+        } catch (ReflectiveOperationException e) {
+            throw new IllegalArgumentException(
+                    "Couldn't create neither a Oracle Mojarra Tag attribute nor an Apache MyFaces TagAttribute", e);
+        }
     }
 
 }
