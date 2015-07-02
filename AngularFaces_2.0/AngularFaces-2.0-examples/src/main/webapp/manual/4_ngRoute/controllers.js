630ab6e10fccc49b15ab1545ec17b104e4e27275
stephanrauh <stephan@beyondjava.de>
2015-07-02 22:56:47 +0200

    Implemented an example for ngRoute

---
+++ AngularFaces_2.0/AngularFaces-2.0-examples/src/main/webapp/manual/4_ngRoute/controllers.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var tutorialControllers = angular.module('tutorialControllers', []);
+
+tutorialControllers.controller('EmptyController', [ '$scope', '$routeParams',
+		function($scope, $routeParams) {
+			$scope.tutorialId = $routeParams.tutorialId;
+
+		} ]);
