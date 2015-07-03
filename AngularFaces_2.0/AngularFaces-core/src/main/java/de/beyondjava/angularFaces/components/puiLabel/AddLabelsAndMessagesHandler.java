d0047164e7b8745844f07eb763be7cf5319a271e
stephanrauh <stephan@beyondjava.de>
2015-07-03 23:25:17 +0200

    Add an option to disable the automatic addition of labels and messages globally #56

--- AngularFaces_2.0/AngularFaces-core/src/main/java/de/beyondjava/angularFaces/components/puiLabel/AddLabelsAndMessagesHandler.java
+++ AngularFaces_2.0/AngularFaces-core/src/main/java/de/beyondjava/angularFaces/components/puiLabel/AddLabelsAndMessagesHandler.java
@@ -36,184 +36,194 @@ import de.beyondjava.angularFaces.core.i18n.I18n;
 import de.beyondjava.angularFaces.core.transformation.AttributeUtilities;
 
 public class AddLabelsAndMessagesHandler extends TagHandler {
-    private I18n i18n = null;
+	private I18n i18n = null;
 
-    boolean addLabels = true;
-    boolean addMessages = true;
+	boolean addLabels = true;
+	boolean addMessages = true;
 
-    public AddLabelsAndMessagesHandler(TagConfig config) {
-        super(config);
-    }
+	public AddLabelsAndMessagesHandler(TagConfig config) {
+		super(config);
+	}
 
-    @Override
-    public void apply(FaceletContext ctx, UIComponent parent) throws IOException {
-        boolean isPostBack=FacesContext.getCurrentInstance().isPostback();
-        String l = AttributeUtilities.getAttributeAsString(parent, "addLabels");
-        if (l != null) {
-            if ("false".equalsIgnoreCase(l))
-                addLabels = false;
-            else {
-                addLabels = true;
-            }
-        }
-        String m = AttributeUtilities.getAttributeAsString(parent, "addMessages");
-        if (m != null) {
-            if ("false".equalsIgnoreCase(m))
-                addMessages = false;
-            else {
-                addMessages = true;
-            }
-        }
+	@Override
+	public void apply(FaceletContext ctx, UIComponent parent) throws IOException {
+		String addLabelsParam = FacesContext.getCurrentInstance().getExternalContext()
+				.getInitParameter("AngularFaces.addLabels");
+		if (null == addLabelsParam || "true".equalsIgnoreCase(addLabelsParam)) {
+			addLabels = true;
+		} else {
+			addLabels = false;
+		}
 
-        if (addLabels || addMessages) {
-            populateTree(parent, addLabels, addMessages, isPostBack);
-        }
-    }
+		boolean isPostBack = FacesContext.getCurrentInstance().isPostback();
+		String l = AttributeUtilities.getAttributeAsString(parent, "addLabels");
+		if (l != null) {
+			if ("false".equalsIgnoreCase(l))
+				addLabels = false;
+		}
+		String addMessagesParam = FacesContext.getCurrentInstance().getExternalContext()
+				.getInitParameter("AngularFaces.addMessages");
+		if (null == addMessagesParam || "true".equalsIgnoreCase(addMessagesParam)) {
+			addMessages = true;
+		} else {
+			addMessages = false;
+		}
+		String m = AttributeUtilities.getAttributeAsString(parent, "addMessages");
+		if (m != null) {
+			if ("false".equalsIgnoreCase(m))
+				addMessages = false;
+		}
 
-    private void populateTree(UIComponent parent, boolean addLabels, boolean addMessages, boolean isPostBack) {
-        List<UIComponent> children = parent.getChildren();
-        
-        for (int index = children.size() - 1; index >= 0; index--) {
-            UIComponent kid = children.get(index);
-            if (kid instanceof UIInput) {
-                if (addMessages)
-                    addSingleMessage(children, index, kid, isPostBack);
-                if (addLabels)
-                    addSingleLabel(children, kid, isPostBack);
-            }
-            String l = AttributeUtilities.getAttributeAsString(kid, "addLabels");
-            if (l != null) {
-                if ("false".equalsIgnoreCase(l))
-                    addLabels = false;
-                else {
-                    addLabels = true;
-                }
-            }
-            String m = AttributeUtilities.getAttributeAsString(kid, "addMessages");
-            if (m != null) {
-                if ("false".equalsIgnoreCase(m))
-                    addMessages = false;
-                else {
-                    addMessages = true;
-                }
-            }
-            populateTree(kid, addLabels, addMessages, isPostBack);
-        }
-    }
+		if (addLabels || addMessages) {
+			populateTree(parent, addLabels, addMessages, isPostBack);
+		}
+	}
 
-    private void addSingleLabel(List<UIComponent> children, UIComponent kid, boolean isPostback) {
-        int index = children.indexOf(kid);
-        boolean needsLabel = true;
-        String addLabelAttribute = AttributeUtilities.getAttributeAsString(kid, "addLabel");
-        if (null != addLabelAttribute && "false".equalsIgnoreCase(addLabelAttribute)) {
-            needsLabel = false;
-        }
+	private void populateTree(UIComponent parent, boolean addLabels, boolean addMessages, boolean isPostBack) {
+		List<UIComponent> children = parent.getChildren();
 
-        if (needsLabel) {
-            String caption = AttributeUtilities.getAttributeAsString(kid, "label");
+		for (int index = children.size() - 1; index >= 0; index--) {
+			UIComponent kid = children.get(index);
+			if (kid instanceof UIInput) {
+				if (addMessages)
+					addSingleMessage(children, index, kid, isPostBack);
+				if (addLabels)
+					addSingleLabel(children, kid, isPostBack);
+			}
+			String l = AttributeUtilities.getAttributeAsString(kid, "addLabels");
+			if (l != null) {
+				if ("false".equalsIgnoreCase(l))
+					addLabels = false;
+				else {
+					addLabels = true;
+				}
+			}
+			String m = AttributeUtilities.getAttributeAsString(kid, "addMessages");
+			if (m != null) {
+				if ("false".equalsIgnoreCase(m))
+					addMessages = false;
+				else {
+					addMessages = true;
+				}
+			}
+			populateTree(kid, addLabels, addMessages, isPostBack);
+		}
+	}
 
-            if (null == caption) {
-                ValueExpression vex = kid.getValueExpression("value");
-                if (null != vex) {
-                    String core = vex.getExpressionString();
-                    caption = NGWordUtiltites.labelFromELExpression(core);
-                }
-            }
-            if (null != caption) {
-                for (int j = 0; j < children.size(); j++) {
-                    UIComponent maybe = children.get(j);
-                    if (maybe instanceof HtmlOutputLabel) {
-                        if (kid.getId().equals(((HtmlOutputLabel) maybe).getFor())) {
-                            needsLabel = false;
-                        }
-                    }
+	private void addSingleLabel(List<UIComponent> children, UIComponent kid, boolean isPostback) {
+		int index = children.indexOf(kid);
+		boolean needsLabel = true;
+		String addLabelAttribute = AttributeUtilities.getAttributeAsString(kid, "addLabel");
+		if (null != addLabelAttribute && "false".equalsIgnoreCase(addLabelAttribute)) {
+			needsLabel = false;
+		}
 
-                }
-                
-                if (isPostback) {
-                    for (int i = 0; i < children.size(); i++) {
-                        UIComponent maybe = children.get(i);
-                        if (maybe instanceof PuiLabel) {
-                            UIComponent forComponent = maybe.findComponent(((PuiLabel) maybe).getFor());
-                            if (kid == forComponent && (index-1) != i) {
-                                needsLabel = false;
-//                                System.out.println("Label at wrong location. Wanted: " + (index-1) + " found at: " + i);
-                                children.remove(i);
-                                children.add(index-1, maybe);
+		if (needsLabel) {
+			String caption = AttributeUtilities.getAttributeAsString(kid, "label");
 
-                            }
-                        }
-                    }
-                }
+			if (null == caption) {
+				ValueExpression vex = kid.getValueExpression("value");
+				if (null != vex) {
+					String core = vex.getExpressionString();
+					caption = NGWordUtiltites.labelFromELExpression(core);
+				}
+			}
+			if (null != caption) {
+				for (int j = 0; j < children.size(); j++) {
+					UIComponent maybe = children.get(j);
+					if (maybe instanceof HtmlOutputLabel) {
+						if (kid.getId().equals(((HtmlOutputLabel) maybe).getFor())) {
+							needsLabel = false;
+						}
+					}
 
-                
-                if (needsLabel) {
-                    HtmlOutputLabel label = new PuiLabel();
-                    label.getPassThroughAttributes().put("for", kid.getClientId());
-                    kid.getAttributes().put("renderLabel", false);
-                    label.setFor(kid.getId());
-                    if (null != caption) {
-                        label.setValue(translate(caption));
-                    }
-                    children.add(index, label);
-                }
-            }
-        }
-    }
+				}
 
-    private String translate(String caption) {
-        if (null == i18n)
-            i18n = (I18n) ELTools.evalAsObject("#{i18n}");
-        if (null == i18n)
-            return caption;
-        return i18n.translate(caption);
-    }
+				if (isPostback) {
+					for (int i = 0; i < children.size(); i++) {
+						UIComponent maybe = children.get(i);
+						if (maybe instanceof PuiLabel) {
+							UIComponent forComponent = maybe.findComponent(((PuiLabel) maybe).getFor());
+							if (kid == forComponent && (index - 1) != i) {
+								needsLabel = false;
+								// System.out.println("Label at wrong location.
+								// Wanted: " + (index-1) + " found at: " + i);
+								children.remove(i);
+								children.add(index - 1, maybe);
 
-    private int addSingleMessage(List<UIComponent> children, int index, UIComponent kid, boolean isPostback) {
-        if (kid instanceof UIInput && (!(kid instanceof PuiSync))) {
-            boolean needsMessage = true;
-            String addMessageAttribute = AttributeUtilities.getAttributeAsString(kid, "addMessage");
-            if (null != addMessageAttribute && "false".equalsIgnoreCase(addMessageAttribute)) {
-                needsMessage = false;
-            }
-            if (needsMessage) {
-                if (index < children.size() - 1) {
-                    UIComponent maybe = children.get(index + 1);
-                    if (maybe instanceof PuiMessage) {
-                        UIComponent forComponent = maybe.findComponent(((UIMessage) maybe).getFor());
-                        if (kid == forComponent) {
-                            needsMessage = false;
-                        }
-                    }
-                }
-                
-                if (needsMessage && isPostback) {
-                    for (int i = 0; i < children.size(); i++) {
-                        UIComponent maybe = children.get(i);
-                        if (maybe instanceof PuiMessage) {
-                            UIComponent forComponent = maybe.findComponent(((UIMessage) maybe).getFor());
-                            if (kid == forComponent) {
-                                needsMessage = false;
-//                                System.out.println("Message at wrong location. Wanted: " + index + " found at: " + i);
-                                children.remove(i);
-                                children.add(index, maybe);
-                                break;
-                            }
-                        }
-                    }
-                }
+							}
+						}
+					}
+				}
 
+				if (needsLabel) {
+					HtmlOutputLabel label = new PuiLabel();
+					label.getPassThroughAttributes().put("for", kid.getClientId());
+					kid.getAttributes().put("renderLabel", false);
+					label.setFor(kid.getId());
+					if (null != caption) {
+						label.setValue(translate(caption));
+					}
+					children.add(index, label);
+				}
+			}
+		}
+	}
 
-                if (needsMessage) {
-                    PuiMessage message;
-                    message = new PuiMessage(); //
-                    message.setFor(kid.getClientId());
-                    message.getPassThroughAttributes().put("for", kid.getClientId());
-                    children.add(index + 1, message);
-                }
-            }
+	private String translate(String caption) {
+		if (null == i18n)
+			i18n = (I18n) ELTools.evalAsObject("#{i18n}");
+		if (null == i18n)
+			return caption;
+		return i18n.translate(caption);
+	}
 
-        }
-        return index;
-    }
+	private int addSingleMessage(List<UIComponent> children, int index, UIComponent kid, boolean isPostback) {
+		if (kid instanceof UIInput && (!(kid instanceof PuiSync))) {
+			boolean needsMessage = true;
+			String addMessageAttribute = AttributeUtilities.getAttributeAsString(kid, "addMessage");
+			if (null != addMessageAttribute && "false".equalsIgnoreCase(addMessageAttribute)) {
+				needsMessage = false;
+			}
+			if (needsMessage) {
+				if (index < children.size() - 1) {
+					UIComponent maybe = children.get(index + 1);
+					if (maybe instanceof PuiMessage) {
+						UIComponent forComponent = maybe.findComponent(((UIMessage) maybe).getFor());
+						if (kid == forComponent) {
+							needsMessage = false;
+						}
+					}
+				}
+
+				if (needsMessage && isPostback) {
+					for (int i = 0; i < children.size(); i++) {
+						UIComponent maybe = children.get(i);
+						if (maybe instanceof PuiMessage) {
+							UIComponent forComponent = maybe.findComponent(((UIMessage) maybe).getFor());
+							if (kid == forComponent) {
+								needsMessage = false;
+								// System.out.println("Message at wrong
+								// location. Wanted: " + index + " found at: " +
+								// i);
+								children.remove(i);
+								children.add(index, maybe);
+								break;
+							}
+						}
+					}
+				}
+
+				if (needsMessage) {
+					PuiMessage message;
+					message = new PuiMessage(); //
+					message.setFor(kid.getClientId());
+					message.getPassThroughAttributes().put("for", kid.getClientId());
+					children.add(index + 1, message);
+				}
+			}
+
+		}
+		return index;
+	}
 }
